87f969f28a4489e0973711d10997258ff3d51a7a
danz_souza <danielsouza@gmail.com>
2026-05-13 18:09:42 -0300

    corigindo as fotos novamente

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let scrollPerClick = 100;
 
 async function scrollSlider() {
   conteudo.forEach((curso) => {
+    const caminhoCorretoIndex = curso.img.replace("../", "./");
     sliders.insertAdjacentHTML(
       "beforeend",
-      `<a href="./pages/curso.html?id=${curso.id}"><img class="img-${curso.id} slider-img shrink-0 w-64 md:w-72 object-cover object-top rounded-xl shadow-lg" src="${curso.img}"></a>`,
+      `<a href="./pages/curso.html?id=${curso.id}"><img class="img-${curso.id} slider-img shrink-0 w-64 md:w-72 object-cover object-top rounded-xl shadow-lg" src="${caminhoCorretoIndex}"></a>`,
     );
 
     
